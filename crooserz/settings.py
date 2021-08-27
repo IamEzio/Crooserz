d3691861ce4848937d1ce41d60efb3df8ce436b8
Anshuman <anshumanmaurya111@gmail.com>
2021-08-27 17:35:53 +0530

    created pages and configured naviagtion links

--- crooserz/settings.py
+++ crooserz/settings.py
@@ -119,3 +119,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'STATIC')
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR,'crooserz/static'),
+]

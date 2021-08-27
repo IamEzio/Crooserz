f93379a8cd55fa7d2da52b7ae0b07ddac578b8c1
Anshuman <anshumanmaurya111@gmail.com>
2021-08-27 20:12:41 +0530

    handled some errors

--- crooserz/settings.py
+++ crooserz/settings.py
@@ -76,8 +76,11 @@ WSGI_APPLICATION = 'crooserz.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'crooserz_db',
+        'USER': 'postgres',
+        'PASSWORD': 'isuperman',
+        'HOST': 'localhost',
     }
 }
 

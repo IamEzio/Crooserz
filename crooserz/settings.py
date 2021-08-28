6539566bf20673d22840574209c1992a4ab46fcd
Anshuman <anshumanmaurya111@gmail.com>
2021-08-28 14:48:15 +0530

    Completed Teams model

--- crooserz/settings.py
+++ crooserz/settings.py
@@ -126,3 +126,7 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'STATIC')
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR,'crooserz/static'),
 ]
+
+# mdeia settings
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_URL = '/media/'
